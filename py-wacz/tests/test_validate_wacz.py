5916304c61341a378f057d285de711d859c2b09f
Emma Dickson <emmadickson@Emmas-MacBook-Pro-2.local>
2020-11-23 14:15:20 -0500

    Improve testing for optional flags
    Supports https://github.com/webrecorder/wacz-format/issues/24

--- py-wacz/tests/test_validate_wacz.py
+++ py-wacz/tests/test_validate_wacz.py
@@ -1,4 +1,4 @@
-import unittest, os, zipfile, sys, gzip, json
+import unittest, os, zipfile, sys, gzip, json, tempfile
 from wacz.main import main
 from wacz.util import support_hash_file
 from frictionless import validate
@@ -10,30 +10,32 @@ TEST_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "fixtures")
 class TestWaczFormat(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        with zipfile.ZipFile(
-            os.path.join(TEST_DIR, "valid_example_1.wacz"), "r"
-        ) as zip_ref:
-            zip_ref.extractall("tests/fixtures/unzipped_wacz_1")
-            zip_ref.close()
+        with tempfile.TemporaryDirectory() as tmpdir:
 
-        with zipfile.ZipFile(
-            os.path.join(TEST_DIR, "valid_example_2.wacz"), "r"
-        ) as zip_ref:
-            zip_ref.extractall("tests/fixtures/unzipped_wacz_2")
-            zip_ref.close()
+            with zipfile.ZipFile(
+                os.path.join(TEST_DIR, "valid_example_1.wacz"), "r"
+            ) as zip_ref:
+                zip_ref.extractall(os.path.join(tmpdir, "unzipped_wacz_1"))
+                zip_ref.close()
 
-        self.validation_class_valid_2 = Validation(
-            os.path.join(TEST_DIR, "valid_example_2.wacz")
-        )
-        self.validation_class_valid_1 = Validation(
-            os.path.join(TEST_DIR, "valid_example_1.wacz")
-        )
-        self.validation_class_valid_0 = Validation(
-            os.path.join(TEST_DIR, "valid_example_0.wacz")
-        )
-        self.validation_class_invalid = Validation(
-            os.path.join(TEST_DIR, "invalid_example_1.wacz")
-        )
+            with zipfile.ZipFile(
+                os.path.join(TEST_DIR, "valid_example_2.wacz"), "r"
+            ) as zip_ref:
+                zip_ref.extractall(os.path.join(tmpdir, "unzipped_wacz_2"))
+                zip_ref.close()
+
+            self.validation_class_valid_2 = Validation(
+                os.path.join(TEST_DIR, "valid_example_2.wacz")
+            )
+            self.validation_class_valid_1 = Validation(
+                os.path.join(TEST_DIR, "valid_example_1.wacz")
+            )
+            self.validation_class_valid_0 = Validation(
+                os.path.join(TEST_DIR, "valid_example_0.wacz")
+            )
+            self.validation_class_invalid = Validation(
+                os.path.join(TEST_DIR, "invalid_example_1.wacz")
+            )
 
     def test_overall_command(self):
         self.assertTrue(
