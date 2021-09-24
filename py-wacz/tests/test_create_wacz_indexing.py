34b004fbf5f87c3fef995dc083eed00387d04f74
Ilya Kreymer <ikreymer@gmail.com>
2021-09-23 21:49:03 -0700

    add tests for --split-seeds

--- py-wacz/tests/test_create_wacz_indexing.py
+++ py-wacz/tests/test_create_wacz_indexing.py
@@ -96,6 +96,12 @@ class TestWaczIndexing(unittest.TestCase):
 
     def test_warc_with_extra_pages(self):
         with tempfile.TemporaryDirectory() as tmpdir:
+            with open(os.path.join(tmpdir, "test-extra-pages.jsonl"), "wt") as fh:
+                fh.write("""\
+{"url": "https://www.iana.org/about"}
+{"url": "https://www.iana.org/protocols"}\
+""")
+
             self.assertEqual(
                 main(
                     [
@@ -105,7 +111,7 @@ class TestWaczIndexing(unittest.TestCase):
                         "-o",
                         os.path.join(tmpdir, "test-extra-pages.wacz"),
                         "-e",
-                        os.path.join(TEST_DIR, "test-extra-pages.jsonl"),
+                        os.path.join(tmpdir, "test-extra-pages.jsonl"),
                         "--detect-pages",
                     ]
                 ),
@@ -139,6 +145,70 @@ class TestWaczIndexing(unittest.TestCase):
                         "pages/pages.jsonl",
                     ],
                 )
+
+    def test_warc_with_extra_pages_via_seeds(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with open(os.path.join(tmpdir, "pages.jsonl"), "wt") as fh:
+                fh.write("""\
+{"url": "https://example.com/", "seed": true}
+{"url": "https://www.iana.org/about"}
+{"url": "https://www.iana.org/protocols"}\
+""")
+
+            self.assertEqual(
+                main(
+                    [
+                        "create",
+                        "-f",
+                        os.path.join(TEST_DIR, "example-iana.warc"),
+                        "-o",
+                        os.path.join(tmpdir, "test-extra-pages.wacz"),
+                        "-p",
+                        os.path.join(tmpdir, "pages.jsonl"),
+                        "--split-seeds",
+                    ]
+                ),
+                0,
+            )
+
+            self.assertEqual(
+                main(
+                    [
+                        "validate",
+                        "-f",
+                        os.path.join(tmpdir, "test-extra-pages.wacz"),
+                    ]
+                ),
+                0,
+            )
+
+            with zipfile.ZipFile(os.path.join(tmpdir, "test-extra-pages.wacz")) as zf:
+                filelist = sorted(zf.namelist())
+
+                # verify pages file added for each list
+                self.assertEqual(
+                    filelist,
+                    [
+                        "archive/example-iana.warc",
+                        "datapackage-digest.json",
+                        "datapackage.json",
+                        "indexes/index.cdx.gz",
+                        "indexes/index.idx",
+                        "pages/extraPages.jsonl",
+                        "pages/pages.jsonl",
+                    ],
+                )
+
+                with zf.open("pages/extraPages.jsonl", "r") as fh:
+                    data = fh.read()
+                    self.assertTrue(b"https://www.iana.org/about" in data)
+                    self.assertTrue(b"https://www.iana.org/protocols" in data)
+
+                with zf.open("pages/pages.jsonl", "r") as fh:
+                    data = fh.read()
+                    self.assertTrue(b"https://example.com/" in data)
+
+
 
     def test_warc_resource_record(self):
         with tempfile.TemporaryDirectory() as tmpdir:
